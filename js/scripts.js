4366e1398423bf0d90d172779596686f4bf007e0
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-01-26 10:28:35 -0800

    Add size option and calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
-function Pizza(){
-  this.toppings = []
-  this.price = 10;
+function Pizza(size){
+  this.toppings = [];
+  this.size = size;
 }
+
 Pizza.prototype.addTopping = function (topping) {
   this.toppings.push(topping);
-};
+}
+
+Pizza.prototype.getPrice = function(){
+  var price = 15;
+  if (this.size === "Small") {
+    price = 10;
+  } else if(this.size === "Medium" ){
+    price = 13;
+  }
+  this.toppings.forEach(function(topping){
+    price += parseInt(topping);
+  });
+  return price;
+}
+
 $(document).ready(function(){
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
-    var pizzaOrder = new Pizza();
+    var pizzaOrder = new Pizza($("#size").val());
     $("input:checkbox[name=toppings]:checked").each(function(){
-      pizzaOrder.addTopping(this.value.replace(/[0-9]/g, ''));
-      pizzaOrder.price += parseInt(this.value);
+      pizzaOrder.addTopping(this.value);
     });
-    $("#pizzaPrice").text(pizzaOrder.price);
+  $("#pizzaPrice").text("The cost of your pizza is $" + pizzaOrder.getPrice())
   });
-
-
 });
